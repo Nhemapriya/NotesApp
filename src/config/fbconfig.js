fc648d76ed146e2c8b71aa346446e8b0590621da
Nhemapriya <hemuhema2000@gmail.com>
2022-01-20 19:47:11 +0530

    config files

--- src/config/fbconfig.js
+++ src/config/fbconfig.js
@@ -4,13 +4,13 @@ import 'firebase/firestore';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDdEbkwgXRCVPElW2Ewz4TejDDbF1MEfZg",
-    authDomain: "hemnote-fa958.firebaseapp.com",
-    databaseURL: "https://hemnote-fa958-default-rtdb.firebaseio.com",
-    projectId: "hemnote-fa958",
-    storageBucket: "hemnote-fa958.appspot.com",
-    messagingSenderId: "303636274931",
-    appId: "1:303636274931:web:5991309f69604f7ffa6186"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   // Initialize Firebase
